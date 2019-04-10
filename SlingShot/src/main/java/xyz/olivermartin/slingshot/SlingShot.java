11f3fe4f2d56acb689ffccdb0b1509093325a548
OllieMartin <olivermartin410@gmail.com>
2019-04-10 20:11:03 +0100

    Refactored ready for a 2.0 release

--- SlingShot/src/main/java/xyz/olivermartin/slingshot/SlingShot.java
+++ SlingShot/src/main/java/xyz/olivermartin/slingshot/SlingShot.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.CopyOption;
 import java.nio.file.Files;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -19,9 +20,7 @@ import net.md_5.bungee.api.chat.BaseComponent;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.config.ServerInfo;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
-import net.md_5.bungee.api.event.PostLoginEvent;
 import net.md_5.bungee.api.event.ServerKickEvent;
-import net.md_5.bungee.api.event.ServerSwitchEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.api.plugin.Plugin;
 import net.md_5.bungee.event.EventHandler;
@@ -29,14 +28,20 @@ import net.md_5.bungee.event.EventPriority;
 
 public class SlingShot extends Plugin implements Listener {
 
+	/* START STATIC */
+
 	private static SlingShot instance;
 	public static File configDir;
 	public static ConfigManager configman;
-	public static boolean debug = false;
 
+	public static boolean debug;
+	public static final String LATEST_VERSION = "2.0";
+	public static final String[] ALLOWED_VERSIONS = {
+			LATEST_VERSION
+	};
 	private static Map<String, Boolean> onlineMap;
 
-	public static final String LATEST_VERSION = "1.2";
+	/* START STATIC METHODS */
 
 	public static void debugMessage(String message) {
 		if (debug) ProxyServer.getInstance().getLogger().info("[DEBUG] " + message);
@@ -50,19 +55,66 @@ public class SlingShot extends Plugin implements Listener {
 		return configman.config.getStringList("target-list");
 	}
 
-	@EventHandler
-	public void onLogin(PostLoginEvent event) {
-		//
+	/* END STATIC */
+
+	public void onEnable() {
+
+		instance = this;
+		configDir = getDataFolder();
+		configman = new ConfigManager();
+
+		debug = false;
+		onlineMap = new HashMap<String, Boolean>();
+
+		/* BSTATS */////////////////////////
+		@SuppressWarnings("unused")
+		Metrics metrics = new Metrics(this);
+		////////////////////////////////////
+
+		// Create plugin folder if it doesn't exist
+		if (!getDataFolder().exists()) {
+			getLogger().info("Creating plugin directory!");
+			getDataFolder().mkdirs();
+		}
+
+		// Start config loading
+		configman.startupConfig();
+
+		// If config isn't an allowed version
+		if (! (Arrays.stream(ALLOWED_VERSIONS).anyMatch(x -> x.equals(configman.config.getString("version"))) ) ) {
+
+			getLogger().warning("[SlingShot] CONFIG VERSION INCORRECT - Your config.yml is outdated. It will be saved as config-old.yml, and a fresh config.yml will be created.");
+
+			File f = new File(SlingShot.configDir + File.separator + "config.yml");
+
+			try {
+				Files.copy(f.toPath(), new File(SlingShot.configDir, "config-old.yml").toPath(), new CopyOption[0]);
+				Files.deleteIfExists(f.toPath());
+				configman.startupConfig();
+			} catch (IOException e) {
+				getLogger().severe("[SlingShot] [ERROR] Could not move old config. Plugin will not work until this config is deleted.");
+				e.printStackTrace();
+				return;
+			}
+
+		}
+
+		getProxy().getPluginManager().registerListener(this, this);
+		getProxy().getPluginManager().registerCommand(this, new SlingShotCommand());
+
 	}
 
-	@EventHandler
-	public void onServerSwitch(ServerSwitchEvent event) {
-		//
+	public void onDisable() {
+		getLogger().info("Thankyou for using SlingShot. Disabling...");
 	}
+
+	/* MAIN PLUGIN BODY */
 
 	@SuppressWarnings("deprecation")
 	@EventHandler(priority = EventPriority.LOWEST)
 	public void onServerKick(ServerKickEvent event) {
+
+		/* Player is being kicked from a server... */
 
 		debugMessage("--- START KICK EVENT ---");
 
@@ -73,13 +125,12 @@ public class SlingShot extends Plugin implements Listener {
 		debugMessage("Kicked From: " + s.getName());
 		debugMessage("Kick Reason: " + event.getKickReason());
 
+		// If event is already cancelled we don't need to do anything here...
 		if (event.isCancelled()) {
 			debugMessage("EVENT IS ALREADY CANCELLED");
 			debugMessage("--- END KICK EVENT ---");
 			return;
 		}
-
-
 
 		// This is a fix for an issue in Waterfall that calls this server kick event when players disconnect from a non-lobby server
 		if (event.getKickReason().contains("[Proxy] Lost connection to server")) {
@@ -88,7 +139,7 @@ public class SlingShot extends Plugin implements Listener {
 			return;
 		}
 
-		// If the player is kicked from a server that should not use slingshot
+		// If the player is kicked from a server that should not use SlingShot lets ignore it too
 		if (configman.config.getStringList("no_slingshot").contains(s.getName())) {
 			debugMessage("Player is kicked from a server that doesnt use slingshot");
 			debugMessage("--- END KICK EVENT ---");
@@ -97,7 +148,7 @@ public class SlingShot extends Plugin implements Listener {
 
 		/* END THE EXIT CONDITIONS */
 
-		// FIND THE FIRST ONLINE TARGET SERVER!
+		// Lets figure out what the best online target server is...
 		Iterator<String> it = getTargetList().iterator();
 
 		boolean found = false;
@@ -133,6 +184,7 @@ public class SlingShot extends Plugin implements Listener {
 
 			});
 
+			// Wait for ping result
 			synchronized (next) {
 				try {
 					next.wait(configman.config.getInt("timeout"));
@@ -141,6 +193,7 @@ public class SlingShot extends Plugin implements Listener {
 				}
 			}
 
+			// If it was a successful ping, we have found our server!
 			if (onlineMap.containsKey(next)) {
 				if (onlineMap.get(next)) {
 					found = true;
@@ -150,43 +203,48 @@ public class SlingShot extends Plugin implements Listener {
 
 		}
 
+		// If none of the target servers responded, there is no where to send the player!
 		if (!found) {
-			// TODO no target servers are online!
-			debugMessage("The target server is not online.");
+
+			debugMessage("No target servers are online.");
 			debugMessage("Disconnect player with reason: " + configman.config.getString("kick-message").replace("%REASON%", BaseComponent.toLegacyText(event.getKickReasonComponent())));
 
 			p.disconnect(TextComponent.fromLegacyText(ChatColor.translateAlternateColorCodes('&', configman.config.getString("kick-message").replace("%REASON%", BaseComponent.toLegacyText(event.getKickReasonComponent())))));
 
-			// TODO do we need this to cancel it?? event.setCancelled(true);
-
 			debugMessage("--- END KICK EVENT ---");
+
 			return;
+
 		}
 
-		// we found the best target server that is online:
+		/* If we get this far then we have identified the best target server */
 
-		// If the player is kicked from the "slingshot" server
+		// If the player is being kicked from this server then we will just disconnect them!
 		if (s.getName().equalsIgnoreCase(target)) {
 
-			debugMessage("Player is being kicked from the slingshot server");
+			debugMessage("Player is being kicked from the target server");
 			debugMessage("Disconnect with reason: " + configman.config.getString("kick-message").replace("%REASON%", BaseComponent.toLegacyText(event.getKickReasonComponent())));
 
 			p.disconnect(TextComponent.fromLegacyText(ChatColor.translateAlternateColorCodes('&', configman.config.getString("kick-message").replace("%REASON%", BaseComponent.toLegacyText(event.getKickReasonComponent())))));
-			//TODO do we need this to cancel it?? event.setCancelled(true);
 
 			debugMessage("--- END KICK EVENT ---");
+
 			return;
 
 		}
 
-		debugMessage("This is a regular kick that needs dealing with by slingshot.");
+		/* If we get this far we need to send the player to the target server */
+
+		debugMessage("This is a regular kick that needs dealing with by SlingShot.");
 
 		debugMessage("The target server is online.");
 
+		// Set the destination server for the cancelled kick
 		event.setCancelServer(ProxyServer.getInstance().getServerInfo(target));
 
 		debugMessage("Player will be connected to the target server on kick.");
 
+		// Notify the player
 		p.sendMessage(TextComponent.fromLegacyText(ChatColor.translateAlternateColorCodes('&', configman.config.getString("message").replace("%REASON%", BaseComponent.toLegacyText(event.getKickReasonComponent())))));
 
 		debugMessage("Player notified with message: " + configman.config.getString("message").replace("%REASON%", BaseComponent.toLegacyText(event.getKickReasonComponent())));
@@ -194,54 +252,6 @@ public class SlingShot extends Plugin implements Listener {
 
 		event.setCancelled(true);
 
-	}
-
-
-	public void onEnable() {
-
-		instance = this;
-		configDir = getDataFolder();
-		configman = new ConfigManager();
-
-		onlineMap = new HashMap<String, Boolean>();
-
-		@SuppressWarnings("unused")
-		Metrics metrics = new Metrics(this);
-
-		if (!getDataFolder().exists()) {
-
-			System.out.println("[SlingShot] Creating plugin directory!");
-			getDataFolder().mkdirs();
-
-		}
-
-		configman.startupConfig();
-
-		if (! (configman.config.getString("version").equals(LATEST_VERSION) )) {
-
-			System.out.println("[SlingShot] CONFIG VERSION INCORRECT - Your config.yml is outdated. It will be saved as config-old.yml, and a fresh config.yml will be created.");
-
-			File f = new File(SlingShot.configDir + "\\config.yml");
-
-			try {
-				Files.copy(f.toPath(), new File(SlingShot.configDir, "config-old.yml").toPath(), new CopyOption[0]);
-				Files.deleteIfExists(f.toPath());
-				configman.startupConfig();
-			} catch (IOException e) {
-				System.out.println("[SlingShot] [ERROR] Could not move old config. Plugin will not work until this config is deleted.");
-				e.printStackTrace();
-				return;
-			}
-
-		}
-
-		getProxy().getPluginManager().registerListener(this, this);
-		getProxy().getPluginManager().registerCommand(this, new SlingShotCommand());
-
-	}
-
-	public void onDisable() {
-		getLogger().info("Thankyou for using SlingShot. Disabling...");
 	}
 
 }
